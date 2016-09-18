1f2d7e0a024aaa5de33fecaf99bd77dd55c83639
jonatan <jonatan.nietoagis@dycec.com>
2016-09-18 18:14:19 +0200

    Stripe test token

--- charges.php
+++ charges.php
@@ -7,14 +7,14 @@
  echo '<p>Start payment</p>';
  // Set your secret key: remember to change this to your live secret key in production
  // See your keys here: https://dashboard.stripe.com/account/apikeys
- \Stripe\Stripe::setApiKey("sk_test_tTyL4fH2P7x2dRYHXjm66tBe");
+ vendor\stripe\stripe-php\lib\Stripe\Stripe::setApiKey("sk_test_tTyL4fH2P7x2dRYHXjm66tBe");
 
  // Get the credit card details submitted by the form
  $token = $_POST['stripeToken'];
 
  // Create a charge: this will charge the user's card
  try {
-   $charge = \Stripe\Charge::create(array(
+   $charge = vendor\stripe\stripe-php\lib\Stripe\Charge::create(array(
      "amount" => 2000, // Amount in cents
      "currency" => "eur",
      "source" => $token,
