d54f64ccc4d089362ac012d68998740890b3c26a
jonatan <jonatan.nietoagis@dycec.com>
2016-09-18 17:01:12 +0200

    First commit

--- blog.php
+++ blog.php
@@ -41,6 +41,28 @@
 
 </head>
 <body>
+
+<?php
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+\Stripe\Stripe::setApiKey("sk_test_tTyL4fH2P7x2dRYHXjm66tBe");
+
+// Get the credit card details submitted by the form
+$token = $_POST['stripeToken'];
+
+// Create a charge: this will charge the user's card
+try {
+$charge = \Stripe\Charge::create(array(
+"amount" => 2000, // Amount in cents
+"currency" => "eur",
+"source" => $token,
+"description" => "Example charge"
+));
+} catch(\Stripe\Error\Card $e) {
+// The card has been declined
+}
+?>
+
 	<!-- HEADER -->
 	<header id="HOME" class="header-one">
 		<div class="">
