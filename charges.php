e3808c0773361f941e8f5d7189ddaf8dd7611671
jonatan <jonatan.nietoagis@dycec.com>
2016-09-18 18:01:08 +0200

    Stripe test token

--- charges.php
+++ charges.php
@@ -4,6 +4,8 @@
  </head>
  <body>
  <?php
+ require __DIR__ . 'vendor/autoload.php';
+ 
  echo '<p>Start payment</p>';
  // Set your secret key: remember to change this to your live secret key in production
  // See your keys here: https://dashboard.stripe.com/account/apikeys
